e5f1270d11f4ac2c63d94fa2caafeba37e538393
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-10-12 07:12:08 +0900

    sketch

---
+++ text-to-clipboard.js
@@ -0,0 +1,24 @@
+let hoge = function(event) {
+        //初期化処理
+        document.removeEventListener("copy", hoge, true);
+        event.preventDefault();
+        let clipboardData = event.clipboardData;
+
+
+        //ここにメイン処理
+
+
+
+        //メイン処理結果をクリップボードに貼り付け
+        clipboardData.setData("text/plain", 'ほげほげほげ');
+
+        //ログ出力
+        console.log('ほげほげほげ');
+
+    }
+
+    //コピーイベントにhogeハンドラをバンドル
+    document.addEventListener("copy", hoge, true)
+
+    //コピーイベント実行
+    document.execCommand("copy")
