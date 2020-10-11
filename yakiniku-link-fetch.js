279e60eaf395afa5ed435a055d7aaef0fe5c44b0
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-10-12 08:02:51 +0900

    sketch

---
+++ yakiniku-link-fetch.js
@@ -0,0 +1,38 @@
+async function hoge(targetUrl) {
+  let targetHtmlText = await getText(targetUrl)
+
+  //console.log(targetHtmlText)
+
+  let links = await extractLinks(targetHtmlText)
+
+  console.log(links)
+
+  async function getText(executeUrl) {
+
+    //サーバーへリクエスト開始
+    let response = await fetch(executeUrl)
+
+    //レスポンスWEBページの取得
+    let htmlText = await response.text()
+
+    return htmlText
+  }
+
+  async function extractLinks(targetHtmlText) {
+
+    // 正規表現の変数化などしていきたい
+    let regptn = '<a.*?>.*?</a>'
+
+    let re = new RegExp(regptn, 'g')
+
+    let match
+    let matches = []
+
+    while ((match = re.exec(targetHtmlText)) != null) {
+      matches.push(match[0])
+    }
+
+    return matches
+  }
+}
+hoge('https://search.rakuten.co.jp/search/mall/%E8%82%89/100227/?v=2')
