00e8f962570e156d0fcdeabb4988472736fc6bcf
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-10-19 07:49:07 +0900

    sketch

--- insert-result-area.js
+++ insert-result-area.js
@@ -4,6 +4,6 @@ if (targetElement !== undefined && targetElement !== null) {
 }
 let div = document.createElement('textarea');
 div.innerHTML = '差し込む文字列';
-div.setAttribute("style", "position:fixed; width:100vh;height: 50vh; top: 10px; right: 10px;overflow: auto;resize: both;");
+div.setAttribute("style", "position:fixed; width:100vh;height: 50vh; top: 100px; right: 100px;overflow: auto;resize: both;background-color: rgb(245,241,208);");
 div.setAttribute('id', 'displayResultsArea');
 document.body.appendChild(div);
