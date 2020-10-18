fc72099dc2382a72e69f724fd35cdc4c2233fab5
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-10-19 00:25:55 +0900

    sketch

--- list-up-instance-methods.js
+++ list-up-instance-methods.js
@@ -1,21 +1,37 @@
-function listUpInstanceMethods(instance) {
-    let resultList = new Set();
-    // グローバルなオブジェクトも受け取れるようにインスタンスからプロトタイプを逆引き
-    let instanceMethodsNameList = Object.getOwnPropertyNames(Object.getPrototypeOf(instance));
-
-    for (let idx = 0; idx < instanceMethodsNameList.length; idx++) {
-        resultList.add(instanceMethodsNameList[idx]);
-    }
-
-    return resultList;
+function getClass(className) {
+  // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/eval#Don't_use_eval_needlessly!
+  return Function("return (" + className + ")")();
 }
 
-let resultList
+async function listUpInstanceMethods(className) {
+  // listUpInstanceMethods("Date")
+  // listUpInstanceMethods("Array")
+  // listUpInstanceMethods("String")
+  // listUpInstanceMethods("Window")
+  // listUpInstanceMethods("Math")
+  let resultList = new Set();
+  let targetClass = getClass(className)
+  let targetInstance
+  try {
+    targetInstance = new targetClass()
+  } catch (error) {
+    try {
+      targetInstance = Object.create(targetClass.prototype)
+    } catch (error) {
+      targetInstance = Object.create(targetClass)
+    }
+  }
+  // グローバルなオブジェクトも受け取れるようにインスタンスからプロトタイプを逆引き
+  let instanceMethodsNameList = Object.getOwnPropertyNames(Object.getPrototypeOf(targetInstance))
 
-resultList = listUpInstanceMethods(new Date());
+  for (let idx = 0; idx < instanceMethodsNameList.length; idx++) {
+    resultList.add(instanceMethodsNameList[idx]);
+  }
 
-console.log(Array.from(resultList));
+  return Array.from(resultList);
+}
 
-resultList = listUpInstanceMethods(window);
 
-console.log(Array.from(resultList));
+let targetClassName = prompt("List Up Instance Methods. Please Input Class Name. Ex.) Date Array String Window Math");
+
+listUpInstanceMethods(targetClassName).then(function(resultList){console.log(resultList)})
