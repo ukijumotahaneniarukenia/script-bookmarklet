b8ebbb969c4dcdf56e0d451080bf2daa94b3a565
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-10-24 14:55:52 +0900

    sketch

---
+++ popupNewWindowThenExecuteScript.js
@@ -0,0 +1,18 @@
+//https://data.city.kyoto.lg.jp/
+
+// サイトによってはユーザーにポップアップを許可してもらう必要がある
+let targetUrlList = [
+  'https://data.city.kyoto.lg.jp/',
+  'https://reverseengineering.stackexchange.com/',
+  'https://qiita.com/danishi/items/42d8adf6291515e62284'
+]
+
+let executeScript = 'console.log("うんこ")'
+
+let targetUrlListCnt = targetUrlList.length
+
+for (let index = 0; index < targetUrlListCnt; index++) {
+  let targetUrl = targetUrlList[index];
+  let newWindowName = 'PopUp' + String(index)
+  window.open(targetUrl,newWindowName,'scrollbars=1,menubar=1,resizable=1,width=300,height=400,left=100,top=100'); // can open
+}
