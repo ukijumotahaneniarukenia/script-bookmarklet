c5ae7a61dd4a6a0b404dfada54b2aa6a54f90bce
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-10-24 12:45:12 +0900

    sketch

---
+++ getJsonDataViaAPI.js
@@ -0,0 +1,10 @@
+let targetUrl = prompt('Please Input Url');
+
+console.log(targetUrl)
+
+async function getData(targetUrl) {
+  const response = await fetch(targetUrl);
+  return response.json();
+}
+
+getData(targetUrl).then(data => {console.log(data)})
