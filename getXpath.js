0368b6b9ff62009d8fb8b6a988ad8ea6a1c574ee
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-10-24 20:56:59 +0900

    sketch

---
+++ getXpath.js
@@ -0,0 +1,166 @@
+function formatDateTime(date, format) {
+    format = format.replace(/yyyy/g, date.getFullYear());
+    format = format.replace(/MM/g, ("0" + (date.getMonth() + 1)).slice(-2));
+    format = format.replace(/dd/g, ("0" + date.getDate()).slice(-2));
+    format = format.replace(/HH/g, ("0" + date.getHours()).slice(-2));
+    format = format.replace(/mm/g, ("0" + date.getMinutes()).slice(-2));
+    format = format.replace(/ss/g, ("0" + date.getSeconds()).slice(-2));
+    format = format.replace(/SSS/g, ("00" + date.getMilliseconds()).slice(-3));
+    return format;
+}
+
+function listUpAllXpath(targetElement, xpath, prevXpath, xpathList) {
+    // タグ名が同一か問わず、同一階層に存在しているすべての子ノードリストを取得
+    let iterator = document.evaluate(
+        xpath,
+        document,
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null
+    ); // それ自身単一の場合ないしそれ自身複数の場合
+
+    for (let childIdx = 0; childIdx < iterator.snapshotLength; childIdx++) {
+        // あれば通るしなければ以下は通らない
+
+        let currentElement = iterator.snapshotItem(childIdx);
+
+        let same_hierarchy_children_list = currentElement.children; // それ自身の配下の子ノードを取得
+
+        for (
+            let hieIdx = 0;
+            hieIdx < same_hierarchy_children_list.length;
+            hieIdx++
+        ) {
+            // あれば通るしなければ以下は通らない
+
+            let childElement = same_hierarchy_children_list[
+                hieIdx
+            ].nodeName.toLocaleLowerCase();
+
+            // 同一タグ名に対して連番を付与するために取得
+            let same_tag_hierarchy_children_list = document.evaluate(
+                prevXpath + "/" + childElement,
+                document,
+                null,
+                XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+                null
+            );
+
+            if (same_tag_hierarchy_children_list.snapshotLength === 1) {
+                // 単一の場合
+
+                xpath = prevXpath + "/" + childElement;
+
+                // 前回訪問済みの場合はスキップ
+                if (xpathList.includes(xpath)) {
+                    continue;
+                }
+
+                xpathList.push(xpath);
+
+                listUpAllXpath(
+                    same_hierarchy_children_list[hieIdx],
+                    xpath,
+                    xpath,
+                    xpathList
+                );
+            } else {
+                // 複数の場合
+
+                for (
+                    let sameIdx = 0;
+                    sameIdx < same_tag_hierarchy_children_list.snapshotLength;
+                    sameIdx++
+                ) {
+                    let currentElement = same_tag_hierarchy_children_list.snapshotItem(
+                        sameIdx
+                    );
+
+                    xpath =
+                        prevXpath +
+                        "/" +
+                        childElement +
+                        "[" +
+                        (sameIdx + 1).toString() +
+                        "]";
+
+                    // 前回訪問済みの場合はスキップ
+                    if (xpathList.includes(xpath)) {
+                        continue;
+                    }
+
+                    xpathList.push(xpath);
+
+                    listUpAllXpath(currentElement, xpath, xpath, xpathList);
+                }
+            }
+        }
+    }
+}
+
+function getAllXpath(entryXpath) {
+    let xpathList = [];
+    let targetElement = document
+        .evaluate(
+            entryXpath,
+            document,
+            null,
+            XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+            null
+        )
+        .snapshotItem(0);
+    let prevXpath = entryXpath;
+
+    xpathList.push(entryXpath);
+
+    listUpAllXpath(targetElement, entryXpath, prevXpath, xpathList);
+
+    let domJsonizeHashList = [];
+    for (let xpathIdx = 0; xpathIdx < xpathList.length; xpathIdx++) {
+        let hash = {};
+        let targetXpath = xpathList[xpathIdx];
+        let iterator = document.evaluate(
+            targetXpath,
+            document,
+            null,
+            XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+            null
+        );
+        let targetElement = iterator.snapshotItem(0);
+        hash = {
+            targetUrl: window.location.href,
+            targetXpath: targetXpath,
+            targetElement: targetElement.outerHTML,
+        };
+        domJsonizeHashList.push(hash);
+    }
+    return domJsonizeHashList;
+}
+
+let targetXpath = prompt(
+    "Please Input EntryXpath. ex) /html or /html/body/div[2]"
+);
+
+let resultList = getAllXpath(targetXpath);
+
+console.log(resultList);
+
+let timeStamp = formatDateTime(new Date(), "yyyy-MM-ddTHH-mm-ss");
+
+let blob = new Blob([JSON.stringify(resultList)], { type: "text/plain" });
+
+let url = URL.createObjectURL(blob);
+
+let a = document.createElement("a");
+
+document.body.appendChild(a);
+
+a.download = "xpath-info-" + timeStamp + ".json";
+
+a.href = url;
+
+a.click();
+
+a.remove();
+
+URL.revokeObjectURL(url);
