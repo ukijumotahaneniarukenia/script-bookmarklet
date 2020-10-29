94b260cf491f2e53c254e80670489df5192e70de
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-10-29 20:16:18 +0900

    Update downloadDomTable2Csv.js

--- downloadDomTable2Csv.js
+++ downloadDomTable2Csv.js
@@ -1,42 +1,82 @@
-let targetTableXpath = prompt(
-    "Please Input TargetTableXpath"
-)
+async function main() {
 
-if (targetTableXpath === undefined || targetTableXpath === '') {
-    alert('Must Input TargetTableXpath')
-    (function(){
-        return
-    })()
+    const targetTableXpath = await checkInputTargetTableXpath()
+
+    console.log(targetTableXpath)
+
+    const downLoadFileName = await checkInputDownLoadFileName()
+
+    console.log(downLoadFileName)
+
+    const targetTableDom = await defineTargetDom(targetTableXpath)
+
+    console.log(targetTableDom)
+
+    await downloadCsv(downLoadFileName)
+
 }
 
-let downLoadFileName = prompt(
-    "Please Input DownLoadFileName"
-)
+async function downloadCsv(downLoadFileName) {
+    // ライブラリの読み込み
+    // https://www.npmjs.com/package/excellentexport
+    let scriptLibrary = document.createElement('script')
+    scriptLibrary.setAttribute('type', 'text/javascript')
+    scriptLibrary.setAttribute('src', 'https://cdn.jsdelivr.net/npm/excellentexport@3.4.3/dist/excellentexport.min.js')
+    document.head.appendChild(scriptLibrary)
 
-let targetTableDom = document.evaluate(targetTableXpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0)
+    // ライブラリの読み込み待ち時間
+    // 3秒後にダウンロード開始
+    setTimeout(function(){
+        let downloadButton = document.createElement('a')
 
-// 対象DOMテーブルに一意のIDを付番
-targetTableDom.setAttribute('id', 'targetTableId')
+        downloadButton.setAttribute('download', downLoadFileName)
 
-// ライブラリの読み込み
-// https://www.npmjs.com/package/excellentexport
-let scriptLibrary = document.createElement('script')
-scriptLibrary.setAttribute('type', 'text/javascript')
-scriptLibrary.setAttribute('src', 'https://cdn.jsdelivr.net/npm/excellentexport@3.4.3/dist/excellentexport.min.js')
-document.head.appendChild(scriptLibrary)
+        downloadButton.setAttribute('href', '#')
 
-let downloadButton = document.createElement('a')
+        downloadButton.setAttribute('onclick', "return ExcellentExport.csv(this, 'targetTableId');")
 
-if (downLoadFileName === undefined || downLoadFileName === '') {
-    downLoadFileName = 'sample.csv'
+        downloadButton.click()
+
+        downloadButton.remove()
+    },3000)
+
 }
 
-downloadButton.setAttribute('download', downLoadFileName)
+async function defineTargetDom(targetTableXpath) {
+    let targetTableDom = document.evaluate(targetTableXpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0)
 
-downloadButton.setAttribute('href', '#')
+    // 対象DOMテーブルに一意のIDを付番
+    targetTableDom.setAttribute('id', 'targetTableId')
 
-downloadButton.setAttribute('onclick', "return ExcellentExport.csv(this, 'targetTableId');")
+    return targetTableDom
+}
 
-downloadButton.click()
+async function checkInputTargetTableXpath() {
+    let targetTableXpath = prompt(
+        "Please Input TargetTableXpath"
+    )
 
-downloadButton.remove()
+    if (targetTableXpath === undefined || targetTableXpath === '') {
+        alert('Must Input TargetTableXpath')
+        (function(){
+            return
+        })()
+    }
+
+    return targetTableXpath
+}
+
+async function checkInputDownLoadFileName() {
+
+    let downLoadFileName = prompt(
+        "Please Input DownLoadFileName"
+    )
+
+    if (downLoadFileName === undefined || downLoadFileName === '') {
+        downLoadFileName = 'sample.csv'
+    }
+
+    return downLoadFileName
+}
+
+main()
