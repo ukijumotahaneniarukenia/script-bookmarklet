149131d5a41efe9c43e6df7aaa0dc9aee85479a8
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-11-13 09:08:43 +0900

    sketch

---
+++ validateW3C.js
@@ -0,0 +1,15 @@
+// https://validator.w3.org/
+
+async function changeLocation() {
+  return new Promise((resolve, reject) => {
+    window.location =
+      "http://validator.w3.org/check?uri=" + escape(window.location);
+    resolve();
+  });
+}
+
+async function main() {
+  await changeLocation();
+}
+
+main();
