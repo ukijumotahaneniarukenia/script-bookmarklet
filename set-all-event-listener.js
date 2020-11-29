503fc5b41b5b57a6004c47d8a381960b17a4909e
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-11-29 17:25:48 +0900

    sketch

---
+++ set-all-event-listener.js
@@ -0,0 +1,52 @@
+// https://github.com/alex2844/js-events
+
+// オブジェクトの全プロパティを取得する関数が必要
+
+// https://stackoverflow.com/questions/11547672/how-to-stringify-event-object
+function eventCatchLogger(event) {
+  console.log(event);
+  console.log(stringifyEvent(event));
+  console.log(stringifyObject(event));
+  console.log(JSON.parse(stringifyEvent(event)));
+  console.log(JSON.parse(stringifyObject(event)));
+}
+
+function stringifyObject(object, depth=0, max_depth=5) {
+  // change max_depth to see more levels, for a touch event, 2 is good
+  if (depth > max_depth)
+      return 'Object';
+
+  const obj = {};
+  for (let key in object) {
+      let value = object[key];
+      if (value instanceof Node)
+          // specify which properties you want to see from the node
+          value = {id: value.id};
+      else if (value instanceof Window)
+          value = 'Window';
+      else if (value instanceof Object)
+          value = stringifyObject(value, depth+1, max_depth);
+
+      obj[key] = value;
+  }
+
+  return depth? obj: JSON.stringify(obj);
+}
+
+function stringifyEvent(e) {
+  const obj = {};
+  for (let k in e) {
+    obj[k] = e[k];
+  }
+  return JSON.stringify(obj, (k, v) => {
+    if (v instanceof Node) return 'Node';
+    if (v instanceof Window) return 'Window';
+    return v;
+  }, ' ');
+}
+
+let allEventList = Object.keys(window).filter((key) => /^on/.test(key));
+
+allEventList.map((event) => {
+  document.body.addEventListener(event.slice(2), eventCatchLogger);
+});
