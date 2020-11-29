c5e1f366a01695e602ca5a8a7bcc98cbbbba9a96
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-11-29 23:00:24 +0900

    sketch

---
+++ copyToClipboard.js
@@ -0,0 +1,9 @@
+let targetText = window.getSelection();
+
+let copyToClipboard = function(e){
+    e.clipboardData.setData("text/plain" , targetText);
+    e.preventDefault();
+    document.removeEventListener("copy", copyToClipboard);
+}
+
+document.addEventListener("copy" , copyToClipboard);
