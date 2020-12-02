c1a6d0bc0c8bb969fc28529729b876452784f834
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-12-03 00:31:40 +0900

    sketch

--- downloadDomTable2Csv.js
+++ downloadDomTable2Csv.js
@@ -1,82 +1,168 @@
-async function main() {
+async function downloadCsv(downLoadFileName) {
+    return new Promise((resolve, reject) => {
+        // ライブラリの読み込み
+        // https://www.npmjs.com/package/excellentexport
+        let scriptLibrary = document.createElement("script");
+        scriptLibrary.setAttribute("type", "text/javascript");
+        scriptLibrary.setAttribute(
+            "src",
+            "https://cdn.jsdelivr.net/npm/excellentexport@3.4.3/dist/excellentexport.min.js"
+        );
+        document.head.appendChild(scriptLibrary);
+        // ライブラリの読み込み待ち時間
+        // 3秒後にダウンロード開始
+        setTimeout(function () {
+            let downloadButton = document.createElement("a");
 
-    const targetTableXpath = await checkInputTargetTableXpath()
+            downloadButton.setAttribute("download", downLoadFileName);
 
-    console.log(targetTableXpath)
+            downloadButton.setAttribute("href", "#");
 
-    const downLoadFileName = await checkInputDownLoadFileName()
+            downloadButton.setAttribute(
+                "onclick",
+                "return ExcellentExport.csv(this, 'targetTableId');"
+            );
 
-    console.log(downLoadFileName)
+            downloadButton.click();
 
-    const targetTableDom = await defineTargetDom(targetTableXpath)
-
-    console.log(targetTableDom)
-
-    await downloadCsv(downLoadFileName)
-
+            downloadButton.remove();
+            let successMessage = "Complete Download";
+            let resultInfo = {
+                status: 0,
+                message: successMessage,
+            };
+            resolve(resultInfo);
+        }, 3000);
+    });
 }
 
-async function downloadCsv(downLoadFileName) {
-    // ライブラリの読み込み
-    // https://www.npmjs.com/package/excellentexport
-    let scriptLibrary = document.createElement('script')
-    scriptLibrary.setAttribute('type', 'text/javascript')
-    scriptLibrary.setAttribute('src', 'https://cdn.jsdelivr.net/npm/excellentexport@3.4.3/dist/excellentexport.min.js')
-    document.head.appendChild(scriptLibrary)
+async function undefineTargetDom(targetTableXpath) {
+    return new Promise((resolve, reject) => {
+        let targetTableDom = document
+            .evaluate(
+                targetTableXpath,
+                document,
+                null,
+                XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+                null
+            )
+            .snapshotItem(0);
+        if (targetTableDom === undefined || targetTableDom === null) {
+            let errorMessage = "Can not define table dom";
+            alert(errorMessage);
+            let errorInfo = {
+                status: 1,
+                message: errorMessage,
+            };
+            reject(errorInfo);
+        }
+        // 対象DOMテーブルに一意のIDを付番
+        targetTableDom.removeAttribute("id");
 
-    // ライブラリの読み込み待ち時間
-    // 3秒後にダウンロード開始
-    setTimeout(function(){
-        let downloadButton = document.createElement('a')
-
-        downloadButton.setAttribute('download', downLoadFileName)
-
-        downloadButton.setAttribute('href', '#')
-
-        downloadButton.setAttribute('onclick', "return ExcellentExport.csv(this, 'targetTableId');")
-
-        downloadButton.click()
-
-        downloadButton.remove()
-    },3000)
-
+        resolve(targetTableDom);
+    });
 }
 
 async function defineTargetDom(targetTableXpath) {
-    let targetTableDom = document.evaluate(targetTableXpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0)
+    return new Promise((resolve, reject) => {
+        let targetTableDom = document
+            .evaluate(
+                targetTableXpath,
+                document,
+                null,
+                XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+                null
+            )
+            .snapshotItem(0);
+        if (targetTableDom === undefined || targetTableDom === null) {
+            let errorMessage = "Can not define table dom";
+            alert(errorMessage);
+            let errorInfo = {
+                status: 1,
+                message: errorMessage,
+            };
+            reject(errorInfo);
+        }
+        // 対象DOMテーブルに一意のIDを付番
+        targetTableDom.setAttribute("id", "targetTableId");
 
-    // 対象DOMテーブルに一意のIDを付番
-    targetTableDom.setAttribute('id', 'targetTableId')
-
-    return targetTableDom
+        resolve(targetTableDom);
+    });
 }
 
 async function checkInputTargetTableXpath() {
-    let targetTableXpath = prompt(
-        "Please Input TargetTableXpath"
-    )
+    return new Promise((resolve, reject) => {
+        let targetTableXpath = prompt("Please Input TargetTableXpath");
 
-    if (targetTableXpath === undefined || targetTableXpath === '') {
-        alert('Must Input TargetTableXpath')
-        (function(){
-            return
-        })()
-    }
+        if (targetTableXpath === undefined || targetTableXpath === "") {
+            let errorMessage = "Must Input TargetTableXpath";
+            alert(errorMessage);
+            let errorInfo = {
+                status: 1,
+                message: errorMessage,
+            };
+            reject(errorInfo);
+        }
 
-    return targetTableXpath
+        let resultInfo = {
+            tableXpath: targetTableXpath,
+        };
+        resolve(resultInfo);
+    });
 }
 
 async function checkInputDownLoadFileName() {
-
-    let downLoadFileName = prompt(
-        "Please Input DownLoadFileName"
-    )
-
-    if (downLoadFileName === undefined || downLoadFileName === '') {
-        downLoadFileName = 'sample.csv'
-    }
-
-    return downLoadFileName
+    return new Promise((resolve, reject) => {
+        let targetDownLoadFileName = prompt(
+            "Please Input DownLoadFileName. Not Include Suffix."
+        );
+        let suffix = ".tsv";
+        let timeStamp = formatDateTime(new Date(), "yyyy-MM-ddTHH-mm-ss");
+        if (targetDownLoadFileName === undefined || targetDownLoadFileName === "") {
+            targetDownLoadFileName = "sample-" + timeStamp + suffix;
+        }
+        let resultInfo = {
+            downLoadFileName: targetDownLoadFileName + "-" + timeStamp + suffix,
+        };
+        resolve(resultInfo);
+    });
 }
 
-main()
+function formatDateTime(date, format) {
+    format = format.replace(/yyyy/g, date.getFullYear());
+    format = format.replace(/MM/g, ("0" + (date.getMonth() + 1)).slice(-2));
+    format = format.replace(/dd/g, ("0" + date.getDate()).slice(-2));
+    format = format.replace(/HH/g, ("0" + date.getHours()).slice(-2));
+    format = format.replace(/mm/g, ("0" + date.getMinutes()).slice(-2));
+    format = format.replace(/ss/g, ("0" + date.getSeconds()).slice(-2));
+    format = format.replace(/SSS/g, ("00" + date.getMilliseconds()).slice(-3));
+    return format;
+}
+
+async function main() {
+    let tableXpathInfo = await checkInputTargetTableXpath();
+
+    console.log(tableXpathInfo);
+
+    let downLoadFileNameInfo = await checkInputDownLoadFileName();
+
+    console.log(downLoadFileNameInfo);
+
+    defineTargetDom(tableXpathInfo.tableXpath)
+        .then((resultInfo) => {
+            console.log(resultInfo);
+            return downloadCsv(downLoadFileNameInfo.downLoadFileName);
+        })
+        .then((resultInfo) => {
+            console.log(resultInfo);
+            return undefineTargetDom(tableXpathInfo.tableXpath);
+        })
+        .then((resultInfo) => {
+            console.log(resultInfo);
+        })
+        .catch((errorInfo) => {
+            console.log(errorInfo);
+        });
+}
+
+main();
