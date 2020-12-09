53808e01c6c9298ee1f1dc82b5fa38dfe00e2a9a
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-12-09 22:42:34 +0900

    sketch

---
+++ fetch-retry.js
@@ -0,0 +1,31 @@
+// https://dev.to/ycmjason/javascript-fetch-retry-upon-failure-3p6g
+async function fetchRetry(url, options, n, cnt) {
+  try {
+    console.log("[ %d ]回目", cnt);
+    if (3 !== getRandom(1, 5)) {
+      throw new Error("Exception");
+    }
+    return await fetch(url, options);
+  } catch (error) {
+    if (n === 1) {
+      throw new Error("Max Retry Count End...");
+    }
+    cnt++;
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        resolve(); // resolve()を返すのがミソ
+      }, 1000)
+    );
+    return await fetchRetry(url, options, n - 1, cnt);
+  }
+}
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+fetchRetry("https://httpbin.org/get", {}, 10, 1).then((response) => {
+  console.log(response);
+});
