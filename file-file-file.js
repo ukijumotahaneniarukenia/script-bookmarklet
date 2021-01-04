b8d2580fda76c275336a3d832fccdc4e89daa467
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2021-01-05 02:12:11 +0900

    Create file-file-file.js

---
+++ file-file-file.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+
+// statXXX系を参考に
+// https://github.com/webpack/webpack/blob/master/bin/webpack.js
+
+function fileRead(targetFileName) {
+  return new Promise((resolve, reject) => {
+
+    fs.readFile(targetFileName, "utf8", (error, data)=>{
+
+      if (error) {
+        console.log(error)
+      }
+
+      console.log(data)
+
+    })
+  })
+}
+
+fileRead("test.json")
