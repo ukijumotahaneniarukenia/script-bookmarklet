374c2903583cc665cebd9e7247323998f6a52382
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-01-09 23:57:55 +0900

    sketch

---
+++ get-registered-all-event-listener.js
@@ -0,0 +1,14 @@
+// https://developers.google.com/web/tools/chrome-devtools/console/utilities#geteventlisteners
+
+// ドキュメントオブジェクトに登録されているすべてのイベントリスナーをリストアップ
+let registeredEventList = getEventListeners(document);
+
+console.log(registeredEventList)
+
+
+// 指定したオブジェクトに登録されているすべてのイベントリスナーをリストアップ
+let targetDom = document.querySelector('form')
+
+let registeredEventList = getEventListeners(targetDom);
+
+console.log(registeredEventList)
