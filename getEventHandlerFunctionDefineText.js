1d2a3f52fb56cea7134e186fa07f70dca3df0769
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-02-03 22:33:57 +0900

    sketch

---
+++ getEventHandlerFunctionDefineText.js
@@ -0,0 +1,21 @@
+// 実行サイトなど
+
+// https://www.youtube.com/watch?v=G8WGFw3iUfE
+
+const getEventHandlerFunctionDefineText = (targetEventName) => {
+  let targetRegisteredEventHandlerInfo = getEventListeners(window)
+  let eventHandlerInfoList = new Array()
+  targetRegisteredEventHandlerInfo[targetEventName].map( item => {
+    let pushItem = {
+      "eventName" : `${targetEventName}`,
+      "eventHandler" : item.listener.toString()
+    }
+    eventHandlerInfoList.push(pushItem)
+  })
+  return eventHandlerInfoList
+}
+
+const result = getEventHandlerFunctionDefineText("scroll")
+
+
+console.log(result)
