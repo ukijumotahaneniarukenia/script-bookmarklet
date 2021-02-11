30565cc98b44d873cd2c0d0e0400cd88ff4825f6
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-02-11 23:44:19 +0900

    sketch

---
+++ sampleESmodule.js
@@ -0,0 +1,15 @@
+export class Unko {
+  unko() {
+    let result = "うんこ"
+    console.log(result)
+    return result
+  }
+
+  addUnko(event, targetText) {
+    console.log("addUnko")
+    console.log(event)
+    let targetDom = document.createElement("div")
+    targetDom.innerHTML = targetText
+    document.body.appendChild(targetDom)
+  }
+}
