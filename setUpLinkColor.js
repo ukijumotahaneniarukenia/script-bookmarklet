1bdfa2b774599b51ebd821233a916ec13d8762ca
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-02-28 17:02:05 +0900

    sketch

--- setUpLinkColor.js
+++ setUpLinkColor.js
@@ -1,4 +1,6 @@
 let targetDom = document.querySelector("ul[id='almanac-sidebar-list']");
 Array.from(targetDom.querySelectorAll("a")).map((item) => {
   item.style.setProperty("color", "#FFE01B");
+  item.style.setProperty("font-size", "18px");
+  item.style.setProperty("font-family", "'Times New Roman', Times, serif");
 });
