3219558d390a2072bd34f980685f40c6286f25b9
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-03-06 17:34:11 +0900

    sketch

--- a.js
+++ a.js
@@ -1,14 +1,6 @@
 // 元ネタ https://www.perimeterx.com/tech-blog/2019/list-every-event-that-exists-in-the-browser/
 // テキストベース https://medium.com/@weizmangal/list-every-event-that-exists-in-the-browser-b771579d9b04
-function getEventList(targetPrototypeObject) {
-  // https://github.com/alex2844/js-events
-  return Object.keys(targetPrototypeObject).filter((key) => /^on/.test(key))
-}
-
-function getPrototypeName(prototypeObject) {
-  return prototypeObject.toString().slice(8).slice(0, -1)
-}
-
+// https://stackoverflow.com/questions/31054910/get-functions-methods-of-a-class
 // https://stackoverflow.com/questions/31158902/is-it-possible-to-sort-a-es6-map-object/51242261
 function sortMapKey(targetMap) {
   return new Map([...targetMap.entries()].sort())
@@ -24,18 +16,7 @@ function main() {
 
     try {
       const windowDefinedObject = window[windowDefinedProperty]
-
-      if (!windowDefinedObject || !windowDefinedObject['prototype']) {
-        // windowにオブジェクトが未定義済みないしはプロトタイプが存在しないものはスキップ
-        continue
-      }
-
-      let prototypeObject = windowDefinedObject['prototype']
-
-      let eventList = getEventList(prototypeObject)
-      if (eventList.length !== 0) {
-        resultMap.set(getPrototypeName(prototypeObject), getEventList(prototypeObject))
-      }
+      resultMap.set(windowDefinedProperty, Object.getOwnPropertyNames(windowDefinedObject['prototype']))
     } catch (error) {}
   }
   return sortMapKey(resultMap)
---
+++ getPrototypePropertyList.js
@@ -0,0 +1,26 @@
+// 元ネタ https://www.perimeterx.com/tech-blog/2019/list-every-event-that-exists-in-the-browser/
+// テキストベース https://medium.com/@weizmangal/list-every-event-that-exists-in-the-browser-b771579d9b04
+// https://stackoverflow.com/questions/31054910/get-functions-methods-of-a-class
+// https://stackoverflow.com/questions/31158902/is-it-possible-to-sort-a-es6-map-object/51242261
+function sortMapKey(targetMap) {
+  return new Map([...targetMap.entries()].sort())
+}
+
+function main(targetPrototypeName) {
+  let resultMap = new Map()
+
+  let windowDefinedPropertyList = Object.getOwnPropertyNames(window)
+
+  for (let i = 0; i < windowDefinedPropertyList.length; i++) {
+    const windowDefinedProperty = windowDefinedPropertyList[i]
+
+    try {
+      const windowDefinedObject = window[windowDefinedProperty]
+      resultMap.set(windowDefinedProperty, Object.getOwnPropertyNames(windowDefinedObject['prototype']))
+    } catch (error) {}
+  }
+
+  return sortMapKey(resultMap.get(targetPrototypeName))
+}
+
+main('Date')
