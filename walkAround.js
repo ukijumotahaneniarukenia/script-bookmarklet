11a007a7aca9b3f1db4e2a28677afbeed34459c1
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2021-03-13 00:43:03 +0900

    Create walkAround.js

---
+++ walkAround.js
@@ -0,0 +1,55 @@
+function getXpath(targetDom) {
+  // 最下位のタグから最上位のタグへさかのぼって処理する
+  if (targetDom && targetDom.parentNode) {
+    let xpath = getXpath(targetDom.parentNode) + "/" + targetDom.tagName;
+    let sameHierarchyChildList = [];
+    for (
+      let childIndex = 0;
+      childIndex < targetDom.parentNode.childNodes.length;
+      childIndex++
+    ) {
+      // 対象のDOMが同一階層に複数存在する要素の一つである場合は何番目かを特定するため、親要素からみた子要素すべてのDOMを保持する
+      let childNode = targetDom.parentNode.childNodes[childIndex];
+
+      if (childNode.tagName == targetDom.tagName) {
+        sameHierarchyChildList.push(childNode);
+      }
+    }
+
+    if (1 < sameHierarchyChildList.length) {
+      // 同一階層に複数存在する要素の一つである場合、インデックス番号を付与する
+      for (
+        let sameHierarchyChildIndex = 0;
+        sameHierarchyChildIndex < sameHierarchyChildList.length;
+        sameHierarchyChildIndex++
+      ) {
+        if (sameHierarchyChildList[sameHierarchyChildIndex] === targetDom) {
+          xpath += "[" + (sameHierarchyChildIndex + 1) + "]";
+          break; // 対象のDOMに対して処理できればよいので、それ以外は処理対象外にする
+        }
+      }
+    }
+
+    return xpath.toLowerCase();
+  } else {
+    return "";
+  }
+}
+
+let walker = document.createTreeWalker(
+  document,
+  NodeFilter.SHOW_ELEMENT,
+  (node) => {
+    return NodeFilter.FILTER_ACCEPT;
+  },
+  true
+);
+
+let resultInfoList = [];
+while (walker.nextNode()) {
+  resultInfoList.push({
+    node: walker.currentNode,
+    xpath: getXpath(walker.currentNode),
+  });
+}
+console.log(resultInfoList);
